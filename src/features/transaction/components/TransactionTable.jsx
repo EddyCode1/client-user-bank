dc9bd693b5b0ab9e7db31bf1e68d7accb3b3cd94
Zeta-code2007 <jzeta-2024452@kinal.edu.gt>
2026-06-30 05:48:25 -0600

    Commit Zeta

--- src/features/transaction/components/TransactionTable.jsx
+++ src/features/transaction/components/TransactionTable.jsx
@@ -25,7 +25,7 @@ export default function TransactionTable({
     }
   };
 
-  // prueba de commit ft/zeta
+  // prueba de commit ft/zeta nuevo 
   const formatCurrency = (amount, currency = 'GTQ') => {
     const validAmount = typeof amount === 'number' ? amount : 0;
     const prefix = currency === 'GTQ' ? 'Q' : '$';
